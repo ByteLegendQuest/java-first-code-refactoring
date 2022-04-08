92c935b9d7006b7fcafbb320f9fdc8b5580a1bd9
onionwing <1069269649@qq.com>
2022-04-08 04:38:58 +0000

    Update at 2022-04-08T04:38:58.370783Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -5,15 +5,6 @@ public class Challenge {
     public static int b = 2;
     public static int c = 3;
 
-    public static print(int num){
-        System.out.println("Start!");
-        System.out.println("Number is: " + num);
-        System.out.println("Number+1 is: " + (num + 1));
-        System.out.println("Number*10 is: " + (num * 10));
-        System.out.println("Number*100 is: " + (num * 100));
-        System.out.println("End!");
-    }
-
     public static void main(String[] args) {
         print(a);
         print(b);
@@ -21,5 +12,12 @@ public class Challenge {
     }
 
     // Complete this method
-    public static void print(int number) {}
+    public static void print(int number) {
+        System.out.println("Start!");
+        System.out.println("Number is: " + number);
+        System.out.println("Number+1 is: " + (number + 1));
+        System.out.println("Number*10 is: " + (number * 10));
+        System.out.println("Number*100 is: " + (number * 100));
+        System.out.println("End!");
+    }
 }
