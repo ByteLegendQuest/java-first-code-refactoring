c230cd6b3b831cd147f8116f93c7b052a5aa78ab
SpaceTimeAdministrator <106767177+SpaceTimeAdministrator@users.noreply.github.com>
2022-06-04 08:18:16 +0000

    Update at 2022-06-04T08:18:16.525386Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,12 +12,12 @@ public class Challenge {
     }
 
     // Complete this method
-    public static void print(int number) {
+     public static void print(int number) {
         System.out.println("Start!");
-        System.out.println("Number is: " + c);
-        System.out.println("Number+1 is: " + (c + 1));
-        System.out.println("Number*10 is: " + (c * 10));
-        System.out.println("Number*100 is: " + (c * 100));
+        System.out.println("Number is: " + number);
+        System.out.println("Number+1 is: " + (number + 1));
+        System.out.println("Number*10 is: " + (number * 10));
+        System.out.println("Number*100 is: " + (number * 100));
         System.out.println("End!");
     }
 }
