32d9a4bd42f441ab51354755bd47166843e576b3
Birlinha <omenaribeiro13@gmail.com>
2022-07-11 03:35:35 +0000

    Update at 2022-07-11T03:35:35.403462Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,30 +6,18 @@ public class Challenge {
     public static int c = 3;
 
     public static void main(String[] args) {
-        print("Start!");
-        print("Number is: " + a);
-        print("Number+1 is: " + (a + 1));
-        print("Number*10 is: " + (a * 10));
-        print("Number*100 is: " + (a * 100));
-        print("End!");
-
-        print("Start!");
-        print("Number is: " + b);
-        print("Number+1 is: " + (b + 1));
-        print("Number*10 is: " + (b * 10));
-        print("Number*100 is: " + (b * 100));
-        print("End!");
-
-        print("Start!");
-        print("Number is: " + c);
-        print("Number+1 is: " + (c + 1));
-        print("Number*10 is: " + (c * 10));
-        print("Number*100 is: " + (c * 100));
-        print("End!");
+        print(a);
+        print(b);
+        print(c);
     }
 
     // Complete this method
-    public static void print(String message) {
-        System.out.println(message);
+    public static void print(int number) {
+        System.out.println("Start!");
+        System.out.println("Number is: " + number);
+        System.out.println("Number+1 is: " + (number + 1));
+        System.out.println("Number*10 is: " + (number * 10));
+        System.out.println("Number*100 is: " + (number * 100));
+        System.out.println("End!");
     }
 }
