b079e9f3eed6d648e688e8cd36b99008badbcf6b
JayeHao <15096048532@163.com>
2022-08-12 09:05:12 +0000

    Update at 2022-08-12T09:05:12.684635Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,28 +6,30 @@ public class Challenge {
     public static int c = 3;
 
     public static void main(String[] args) {
-        System.out.println("Start!");
-        System.out.println("Number is: " + a);
-        System.out.println("Number+1 is: " + (a + 1));
-        System.out.println("Number*10 is: " + (a * 10));
-        System.out.println("Number*100 is: " + (a * 100));
-        System.out.println("End!");
+        print("Start!");
+        print("Number is: " + a);
+        print("Number+1 is: " + (a + 1));
+        print("Number*10 is: " + (a * 10));
+        print("Number*100 is: " + (a * 100));
+        print("End!");
 
-        System.out.println("Start!");
-        System.out.println("Number is: " + b);
-        System.out.println("Number+1 is: " + (b + 1));
-        System.out.println("Number*10 is: " + (b * 10));
-        System.out.println("Number*100 is: " + (b * 100));
-        System.out.println("End!");
+        print("Start!");
+        print("Number is: " + b);
+        print("Number+1 is: " + (b + 1));
+        print("Number*10 is: " + (b * 10));
+        print("Number*100 is: " + (b * 100));
+        print("End!");
 
-        System.out.println("Start!");
-        System.out.println("Number is: " + c);
-        System.out.println("Number+1 is: " + (c + 1));
-        System.out.println("Number*10 is: " + (c * 10));
-        System.out.println("Number*100 is: " + (c * 100));
-        System.out.println("End!");
+        print("Start!");
+        print("Number is: " + c);
+        print("Number+1 is: " + (c + 1));
+        print("Number*10 is: " + (c * 10));
+        print("Number*100 is: " + (c * 100));
+        print("End!");
     }
 
     // Complete this method
-    public static void print(int number) {}
+    public static void print(String number) {
+        System.out.println(number);
+    }
 }
