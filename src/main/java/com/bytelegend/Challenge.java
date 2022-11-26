df38f0eceaa197f16439ed40204e1c531447ddfa
jenny-2233 <822204051@qq.com>
2022-11-26 08:52:24 +0000

    Update at 2022-11-26T08:52:24.817633Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,33 +1,26 @@
 package com.bytelegend;
 
-public class Challenge {
+
+
+public class Main {
     public static int a = 1;
     public static int b = 2;
     public static int c = 3;
 
     public static void main(String[] args) {
-        System.out.println("Start!");
-        System.out.println("Number is: " + a);
-        System.out.println("Number+1 is: " + (a + 1));
-        System.out.println("Number*10 is: " + (a * 10));
-        System.out.println("Number*100 is: " + (a * 100));
-        System.out.println("End!");
+        print(a);
+        print(b);
+        print(c);
 
-        System.out.println("Start!");
-        System.out.println("Number is: " + b);
-        System.out.println("Number+1 is: " + (b + 1));
-        System.out.println("Number*10 is: " + (b * 10));
-        System.out.println("Number*100 is: " + (b * 100));
-        System.out.println("End!");
-
-        System.out.println("Start!");
-        System.out.println("Number is: " + c);
-        System.out.println("Number+1 is: " + (c + 1));
-        System.out.println("Number*10 is: " + (c * 10));
-        System.out.println("Number*100 is: " + (c * 100));
-        System.out.println("End!");
     }
 
     // Complete this method
-    public static void print(int number) {}
+    public static void print(int i) {
+        System.out.println("Start!");
+        System.out.println("Number is: " + i);
+        System.out.println("Number+1 is: " + (i + 1));
+        System.out.println("Number*10 is: " + (i * 10));
+        System.out.println("Number*100 is: " + (i * 100));
+        System.out.println("End!");
+    }
 }
