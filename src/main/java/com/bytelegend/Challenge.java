d5b152342a1f792931b92f8139bac76eabaccfd4
TheTenthYama <1355794322@qq.com>
2023-01-11 07:22:29 +0000

    Update at 2023-01-11T07:22:29.343153Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,7 +6,6 @@ public class Challenge {
     public static int c = 3;
 
     public static void main(String[] args) {
-        public static void main(String[] args) {
         print(a);
         print(b);
         print(c);
